cb5e4125812a2ec5d90366d598b520ddbc2c630b
Tyler Hinojosa <tyler.hinojosa@okstate.edu>
2019-01-30 19:17:16 -0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main(){
+	cout << "Week 2 Lab";
+	int bob;
+	int john;
+	
+	bob = 42;
+	john = 32;
+	int jimbob = bob + john;
+	
+	cout << "Bob and John together are";
+	cout << jimbob << "years old" << endl;
+	
+	return 0;
+}
