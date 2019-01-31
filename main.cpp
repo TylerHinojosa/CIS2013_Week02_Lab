4de8661e8925a760428a745f8fd9b9e08393fba2
Tyler Hinojosa <tyler.hinojosa@okstate.edu>
2019-01-30 19:48:13 -0600

    Testing boolean operations

--- main.cpp
+++ main.cpp
@@ -1,16 +1,29 @@
 #include <iostream>
 using namespace std;
 int main(){
-	cout << "Week 2 Lab";
-	int bob;
-	int john;
+	cout << "Week 2 Lab:" << endl;
 	
-	bob = 42;
-	john = 32;
+	int bob = 42;
+	int john = 32;
 	int jimbob = bob + john;
+	short rachel = 10;
+	short luke = 7;
+	float pie = 3.14;
+	char middle = 'M';
+	bool shouldWe = false;
 	
-	cout << "Bob and John together are";
-	cout << jimbob << "years old" << endl;
+	if (jimbob <= luke){
+		cout << "The result was true" << endl;
+	} else {
+		cout << "False!!!!" << endl;
+	}
+	
+	cout << jimbob << " is bob and jim added" << endl;
+	cout << bob - john << " is bob and jim subtracted" << endl;
+	cout << bob/john << " is bob and jim divided" << endl;
+	cout << bob * john <<" is bob and jim multiplied" << endl;
+	cout <<bob % john << " is bob and modulas jim" << endl;
+	
 	
 	return 0;
 }
